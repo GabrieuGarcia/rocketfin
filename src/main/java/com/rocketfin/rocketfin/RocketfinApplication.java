36a4af0425e888cfc870ef31fceb205a9c790de1
Gabriel <contato.gabrielgarcia@gmail.com>
2020-02-17 00:07:16 -0300

    Start of the Project

---
+++ src/main/java/com/rocketfin/rocketfin/RocketfinApplication.java
@@ -0,0 +1,13 @@
+package com.rocketfin.rocketfin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RocketfinApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RocketfinApplication.class, args);
+    }
+
+}
